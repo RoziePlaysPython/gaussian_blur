c7bb1d708f4632f47ee5f9365ca8e22ea1bdb008
RoziePlaysPython <tfatugava@gmail.com>
2022-03-17 11:51:03 +0300

    Wrote apply_weight function

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from sys import argv
 from PIL import Image
 
+#TODO: generate kernel from a function
 kernel = [
         [1, 2, 1],
         [2, 4, 2],
         [1, 2, 1],
         ]
 scope_size = len(kernel)
-# average_dividor = sum([sum([weight for weight in row]) for row in kernel])
+average_dividor = sum([sum([weight for weight in row]) for row in kernel])
 
 # Open image by path and extract nessesary data
 def open_image(path):
@@ -24,6 +24,8 @@ def modify_scope(X, Y, sizeX, sizeY, pixels, scope_size):
     for dY in range(-scope_delta, scope_delta+1):
         scope.append([])
         for dX in range(-scope_delta, scope_delta+1):
+            # If searching pixel out of borders, move it to the closest border pixel
+            # Usage of "while" allows extendability (if scope>3, still works as intended)
             tX, tY = dX, dY
             while X+tX < 0:
                 tX+=1
@@ -33,25 +35,30 @@ def modify_scope(X, Y, sizeX, sizeY, pixels, scope_size):
                 tY+=1
             while Y+tY > sizeY:
                 tY-=1
-            print(dX, dY, 'before extending')
-            print(tX, tY, 'after extending')
-            scope[row_cnt].append(pixels[Y+tY][X+tX]) # later on this will be changed to pixels[x, y] format
+            # print(dX, dY, 'before extending')
+            # print(tX, tY, 'after extending')
+            scope[row_cnt].append(pixels[X+tX, Y+tY]) # later on this will be changed to pixels[x, y] format
                                                       # due to how pixels object works
         print(row_cnt, scope[row_cnt])
         row_cnt+=1
     return scope # reminder that each pixel in scope is a 3-item array on its own
 
 # Applies weights to scope
-# def apply_weight(scope, kernel):
- 
+def apply_weight(scope, kernel):
+    weighted_scope = []
+    for Y in range(len(scope)):
+        weighted_scope.append([])
+        for X in range(len(scope[Y])):
+            pixel = scope[Y][X]
+            weighted_pixel = tuple([pixel[channel]*kernel[Y][X] for channel in range(len(pixel))])
+            weighted_scope[Y].append(weighted_pixel)
+        print(Y, weighted_scope[Y])
+    return weighted_scope
+
 
  #testing zone
 
-pixels = [
-           [0, 1, 2, 3 ],
-           [4, 5, 6, 7 ],
-           [8, 9, 10,11],
-           [12,13,14,15],
-         ]
-print(modify_scope(0, 0, 3, 3, pixels, 5))
-
+if __name__=="__main__":
+    from sys import argv
+    sizeX, sizeY, pixels = open_image(path = argv[1])
+    print(apply_weight(modify_scope(0, 0, sizeX, sizeY, pixels, 3), kernel))
